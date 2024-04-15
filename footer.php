2c5585ad4f219b64e343e3cc5e0825db23553404
lorena-ipsum <lorenanbo@gmail.com>
2024-04-15 20:05:22 +0200

    configuration de base dans les fichiers du theme

---
+++ footer.php
@@ -0,0 +1,9 @@
+</body>
+    <footer>
+        <ul>
+            <li><a href="ops">Mentions Legales</a></li>
+            <li><a href="ops">Vie Privée</a></li>
+            <li>Tout droit réservés</li>
+        </ul>
+    <footer>
+</html>
